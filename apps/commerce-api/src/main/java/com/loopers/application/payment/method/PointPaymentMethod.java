248b50a2854bcab20db1bec4411b2aaae87798dc
oong00 <oongdeveloper@gmail.com>
2025-08-23 03:59:46 +0900

    기존 코드 수정

--- apps/commerce-api/src/main/java/com/loopers/application/payment/method/PointPaymentMethod.java
+++ apps/commerce-api/src/main/java/com/loopers/application/payment/method/PointPaymentMethod.java
@@ -5,11 +5,10 @@ import com.loopers.domain.payment.Payment;
 import com.loopers.domain.payment.PaymentCommand;
 import com.loopers.domain.point.PointCommand;
 import com.loopers.domain.point.PointService;
-import com.loopers.domain.point.exception.InsufficientPointsException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PointPaymentMethod extends AbstractPaymentMethod{
+public class PointPaymentMethod extends AbstractPaymentMethod {
     private final PointService pointService;
 
     public PointPaymentMethod(PointService pointService) {
@@ -23,27 +22,31 @@ public class PointPaymentMethod extends AbstractPaymentMethod{
 
     @Override
     PaymentResult doPay(PaymentCommand command) {
-        PaymentResult paymentResult = new PaymentResult(
-                command.getOrderId(),
-                command.getIdempotencyKey(),
-                command.getAmount(),
-                command.getMethod().name()
-                );
-
         try{
             pointService.deduct(PointCommand.Deduct.of(command.getUserId(), command.getAmount()));
 
-            paymentResult.setSuccess(true);
-            paymentResult.setStatus(PaymentResult.Status.COMPLETED);
-        } catch (InsufficientPointsException e){
-            paymentResult.setSuccess(false);
-            paymentResult.setStatus(PaymentResult.Status.FAILED);
-            paymentResult.setReason(e.getMessage());
+            return new PaymentResult(
+                    command.getOrderId(),
+                    command.getIdempotencyKey(),
+                    command.getAmount(),
+                    command.getMethod(),
+                    PaymentResult.Status.COMPLETED,
+                    "정상 결제되었습니다.",
+                    null,
+                    null
+            );
         } catch (RuntimeException e){
-            paymentResult.setSuccess(false);
-            paymentResult.setStatus(PaymentResult.Status.SYSTEM_FAILED);
+            return new PaymentResult(
+                    command.getOrderId(),
+                    command.getIdempotencyKey(),
+                    command.getAmount(),
+                    command.getMethod(),
+                    PaymentResult.Status.FAILED,
+                    e.getMessage(),
+                    null,
+                    null
+            );
         }
-        return paymentResult;
     }
 
 }
