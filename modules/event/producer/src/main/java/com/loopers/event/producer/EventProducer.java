ae40695f1c08e3be663f1436e0f7cb9a264b86f8
oong00 <oongdeveloper@gmail.com>
2025-09-06 03:26:59 +0900

    Kafka 설정 오류 수정 (kafka.yml 등록)

--- modules/event/producer/src/main/java/com/loopers/event/producer/EventProducer.java
+++ modules/event/producer/src/main/java/com/loopers/event/producer/EventProducer.java
@@ -23,7 +23,6 @@ class EventProducer {
 
     public void send(EventEnvelop<?> event){
         try {
-            log.info("여기까진 온게 맞나? {}", event);
             kafkaTemplate.send(event.getTopic(), event.getEventId(), event.toJson())
                     .get(1L, TimeUnit.MINUTES);
         } catch (InterruptedException | ExecutionException e) {
